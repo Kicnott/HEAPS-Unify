0c5652b557f961ea1d698d9b15f7deef9830832b
Nicholas <nicholasang.sg@gmail.com>
2025-06-13 14:49:41 +0800

    added Mon-Sun row

--- Unify/src/components/mainCalendar.jsx
+++ Unify/src/components/mainCalendar.jsx
@@ -6,6 +6,12 @@ export const MainCalendar = ({children, baseDate}) => {
     // console.log("baseDate:" + baseDate )
     // console.log("dateIndex" + dateIndex)
     let calendarBoxes = [];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+    for (let i = 0; i < 7; i++){
+        calendarBoxes.push(<button key={days[1]}>{days[i]}</button>) // Add Day Names on the top row
+    }
+
     for (let i = 0; i < 35; i++){
         calendarBoxes.push(<button key={i}>{children}{dateIndex.toLocaleDateString()}</button>) // Button functionality to be added
         dateIndex.setDate(dateIndex.getDate() + 1)
