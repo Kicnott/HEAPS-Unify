4c73127877c5502b886f87386837648c6705d979
Nicholas <nicholasang.sg@gmail.com>
2025-06-13 16:10:25 +0800

    tested date_headers colours, functionality to modify boxes

--- Unify/src/components/Calender_Date_Box.jsx
+++ Unify/src/components/Calender_Date_Box.jsx
@@ -10,3 +10,16 @@ export const Calender_Date_Box = ({onClick, children}) => {
     </button>
   )
 }
+
+export const Calender_Date_Headers = ({onClick, children}) => {
+  let button_style = {
+    color : 'yellow',
+    'background-color': 'brown'
+  }
+
+  return (
+    <button onClick={onClick} style={button_style}>
+        {children}
+    </button>
+  )
+}
--- Unify/src/components/mainCalendar.jsx
+++ Unify/src/components/mainCalendar.jsx
@@ -1,4 +1,5 @@
 import { Calender_Date_Box } from './Calender_Date_Box.jsx'
+import { Calender_Date_Headers } from './Calender_Date_Box.jsx'
 import '../styles/mainCalendar.css'
 
 export const MainCalendar = ({children, baseDate}) => {
@@ -9,7 +10,7 @@ export const MainCalendar = ({children, baseDate}) => {
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
     for (let i = 0; i < 7; i++){
-        calendarBoxes.push(<Calender_Date_Box key={days[i]}>{days[i]}</Calender_Date_Box>) // Add Day Names on the top row
+        calendarBoxes.push(<Calender_Date_Headers key={days[i]}>{days[i]}</Calender_Date_Headers>) // Add Day Names on the top row
     }
 
     for (let i = 0; i < 35; i++){
