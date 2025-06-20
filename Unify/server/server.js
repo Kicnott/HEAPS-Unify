f7296227982205ea155bdfc62edd6dc8c2cd7fb5
theejr <jinrae.lee.2024@computing.smu.edu.sg>
2025-06-20 21:42:42 +0800

    Added sequelize(??)

--- Unify/server/server.js
+++ Unify/server/server.js
@@ -1,8 +1,12 @@
-const express = require('express') // Express.js
-const cors = require('cors') // Cross-Origin Resource Sharing: To configure requests to the server; We do not need it until we deploy stuff
+import express from 'express' // Express.js
+import cors from 'cors' // Cross-Origin Resource Sharing: To configure requests to the server; We do not need it until we deploy stuff
 const app = express() // Creates a variable of the Express app
 const port = 8888 // Defines the port number as 8888 for the huat
-const pool = require('./db.js') // Defines the connection pool for the database
+import pool from './db.js' // Defines the connection pool for the database
+import sequelize from './sequelize.js'
+
+
+
 
 app.use(cors()) // Makes the Express app use cors
 app.use(express.json()) // Makes the Express app read incoming json data, which is (probably??) what we will use
@@ -25,6 +29,6 @@ app.listen(port, () => {
 }) // Starts the server and sends the message when there are any requests from port 8888
 
 app.get('/api/message', (req, res) => {
-    res.json({ message: 'Hello the server is working'})
-    console.log('A request hath been made')
+  res.json({ message: 'Hello the server is working' })
+  console.log('A request hath been made')
 }) // Tests connection with front-end; Go to test-server page
