66d3a59d837665c6465ca80c5f436b3198cb42b8
PY <ppy@gmail.com>
2025-06-20 22:24:54 +0800

    server test

--- Unify/server/db.js
+++ Unify/server/db.js
@@ -1,10 +1,17 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'Unify',
+    // password: 'password',
+    // port: 5432
+    user: 'postgres.jcozwjesouhvqxgbdijp',
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
     database: 'Unify',
-    password: 'password',
+    password: 'UnifyHeap2025',
     port: 5432
 })
+
+
 
 module.exports = pool
