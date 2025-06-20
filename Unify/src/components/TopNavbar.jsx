c830f5c5a27314a09890549f38232c476f09132e
shubangirl <shubhangiskps@gmail.com>
2025-06-20 22:01:51 +0800

    Changed header color

--- Unify/src/components/TopNavbar.jsx
+++ Unify/src/components/TopNavbar.jsx
@@ -1,26 +1,10 @@
-import React from 'react'
+import React from 'react';
+import "../styles/TopNavbar.css";
 
-// Top Navigation Bar to hold other buttons, looks fugly, will fix later
-export const TopNavbar = ({children}) => {
+export const TopNavbar = ({ children }) => {
     return (
-        <div style={{
-            height: '60px',
-            background: 'LightGray',
-            borderBottom: '1px solid #ddd',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: '0 20px',
-            marginBottom: '10px',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            zIndex: 1000,   
-        }}
-        >
-        {children}
-
+        <div className="topnavbar">
+            {children}
         </div>
-    )
-}
+    );
+};
