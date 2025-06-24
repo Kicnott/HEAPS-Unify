492ea18255ec46bdeb49c5c5ff912e562de213c0
Nicholas <nicholasang.sg@gmail.com>
2025-06-24 11:01:51 +0800

    forgot to switch back to main db, commented main db details in db.js

--- Unify/server/db.js
+++ Unify/server/db.js
@@ -1,10 +1,17 @@
 import { Pool } from 'pg'
 const pool = new Pool({
-    user: 'nicholas',
-    host: 'localhost',
-    database: 'Unify-Test',
-    password: 'root',
+    user: 'postgres.jcozwjesouhvqxgbdijp',
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+    database: 'Unify',
+    password: 'UnifyHeap2025',
     port: 5432
 })
+
+/*  Main db details
+    user: 'postgres.jcozwjesouhvqxgbdijp',
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+    database: 'Unify',
+    password: 'UnifyHeap2025',
+    port: 5432 */
 
 export default pool
